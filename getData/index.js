82aec9d16fefec554065d1ec3dbf3e7d17c3177b
Tierney Cyren <hello@bnb.im>
2019-05-05 16:40:21 -0400

    Fix spacing on getData/index.js

--- getData/index.js
+++ getData/index.js
@@ -1,20 +1,20 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-    context.res = {
-        status: 200,
-        body: context.bindings.datajson
-    }
-
-    // logging API hits
-    const hits = context.bindings.hitsjsonIn
-    const functionName = 'data' // this needs to be changed per function
-    const functionHits = typeof hits.data === 'number' ? hits.data : 0 // this needs to be changed per function
-    const totalhits = hits.total
-  
-    const additions = {
-      [functionName]: functionHits + 1,
-      "total": totalhits + 1
-    }
-  
-    context.bindings.hitsjsonOut = Object.assign(hits, additions)
+  context.log('JavaScript HTTP trigger function processed a request.');
+  context.res = {
+      status: 200,
+      body: context.bindings.datajson
   }
+
+  // logging API hits
+  const hits = context.bindings.hitsjsonIn
+  const functionName = 'data' // this needs to be changed per function
+  const functionHits = typeof hits.data === 'number' ? hits.data : 0 // this needs to be changed per function
+  const totalhits = hits.total
+
+  const additions = {
+    [functionName]: functionHits + 1,
+    "total": totalhits + 1
+  }
+
+  context.bindings.hitsjsonOut = Object.assign(hits, additions)
+}
